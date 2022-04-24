e8941d16048bdec886d9fd720eaea6e1d772c6bb
Vasilii Zalizniak <vzalizniak31@gmail.com>
2022-04-24 21:41:43 +0300

    Package npm with necessary files was created

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const sum = (x, y) => x + y;
+const subtr = (x, y) => x - y;
+const mult = (x, y) => x * y;
+
+console.log(sum(5, 3));
+console.log(subtr(5, 3));
+console.log(mult(5, 3));
